7dea8398a87ee5b41c90c2efe41785883ceb35e3
ziggoon <79215783+ziggoon@users.noreply.github.com>
2026-02-20 21:01:43 -0600

    xd

--- scripts/chroot.sh
+++ scripts/chroot.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 chroot_config() {
-	arch-chroot /mnt <<CHROOTEOF
+	arch-chroot /mnt <<-CHROOTEOF
 	ln -sf /usr/share/zoneinfo/America/Chicago /etc/localtime
 	hwclock --systohc
 
@@ -9,50 +9,50 @@ chroot_config() {
 	locale-gen
 	echo "LANG=en_US.UTF-8" > /etc/locale.conf
 	echo "KEYMAP=us" > /etc/vconsole.conf
-
 	echo "$HOSTNAME" > /etc/hostname
-	cat <<EOF > /etc/hosts
+
+	cat > /etc/hosts <<-HOSTS
 	127.0.0.1   localhost
 	::1         localhost
 	127.0.1.1   $HOSTNAME.localdomain $HOSTNAME
-	EOF
+	HOSTS
 
 	sed -i 's/^HOOKS=.*/HOOKS=(base udev autodetect microcode modconf kms keyboard keymap consolefont block encrypt filesystems fsck)/' /etc/mkinitcpio.conf
 	mkinitcpio -P
 
 	bootctl install
 
-	cat <<EOF > /boot/loader/loader.conf
+	cat > /boot/loader/loader.conf <<-LOADER
 	default arch.conf
 	timeout 3
 	console-mode max
 	editor  no
-	EOF
+	LOADER
 
-	UUID="$(blkid -s UUID -o value "${PART_LUKS}")"
-	cat <<EOF > /boot/loader/entries/arch.conf
+	UUID="\$(blkid -s UUID -o value ${PART_LUKS})"
+
+	cat > /boot/loader/entries/arch.conf <<-ARCHCONF
 	title   $HOSTNAME
 	linux   /vmlinuz-linux
 	initrd  /initramfs-linux.img
-	options cryptdevice=UUID=$UUID:cryptroot root=/dev/mapper/cryptroot rootflags=subvol=@ rw
-	EOF
+	options cryptdevice=UUID=\$UUID:cryptroot root=/dev/mapper/cryptroot rootflags=subvol=@ rw
+	ARCHCONF
 
-	cat <<EOF > /boot/loader/entries/arch-fallback.conf
+	cat > /boot/loader/entries/arch-fallback.conf <<-ARCHFB
 	title   $HOSTNAME (fallback)
 	linux   /vmlinuz-linux
 	initrd  /initramfs-linux-fallback.img
-	options cryptdevice=UUID=$UUID:cryptroot root=/dev/mapper/cryptroot rootflags=subvol=@ rw
-	EOF
+	options cryptdevice=UUID=\$UUID:cryptroot root=/dev/mapper/cryptroot rootflags=subvol=@ rw
+	ARCHFB
 
-	cat <<EOF > /etc/systemd/network/20-wireless.network
+	cat > /etc/systemd/network/20-wireless.network <<-NETWORK
 	[Match]
 	Name=wlan0
-
 	[Network]
 	DHCP=yes
 	DNS=1.1.1.1
 	DNS=8.8.8.8
-	EOF
+	NETWORK
 
 	systemctl enable iwd systemd-networkd systemd-resolved
 	ln -sf /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
@@ -62,12 +62,11 @@ chroot_config() {
 	echo "$USERNAME:$PASSWORD" | chpasswd
 	sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
 
-	su $USERNAME
-	cd "/home/$USERNAME"
-	git clone https://aur.archlinux.org/yay.git
-	cd yay
-	makepkg -si
-
-	exit
+	runuser -u "$USERNAME" -- bash -c '
+	    cd /home/$USERNAME
+	    git clone https://aur.archlinux.org/yay.git
+	    cd yay
+	    makepkg -si --noconfirm
+	'
 	CHROOTEOF
 }
